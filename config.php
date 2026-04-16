14d0b7227f6782a3261f3be9e5c8a3b592ae7374
Niyo <cybergoth@live.ie>
2026-04-16 20:37:58 +0100

    Update config.php
    Swapped out hardcoded vars for env vars

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
 return [
-    'admin_password' => 'bce16e140b19ae56297e86',
-
-   	'turnstile_secret' => '0x4AAAAAAC-NnvMkLdWI9DdbC3jFlfhQ5d0',
-	'turnstile_site' => '0x4AAAAAAC-Nngrukiqg-dqQ',
-
-    'rate_limit_seconds' => 10,
-    'max_entries' => 50
+    'admin_password' => getenv('ADMIN_PASSWORD') ?: 'bce16e140b19ae56297e86',
+    
+    'turnstile_secret' => getenv('TURNSTILE_SECRET') ?: '0x4AAAAAAC-NnvMkLdWI9DdbC3jFlfhQ5d0',
+    'turnstile_site' => getenv('TURNSTILE_SITE') ?: '0x4AAAAAAC-Nngrukiqg-dqQ',
+    
+    'rate_limit_seconds' => (int)(getenv('RATE_LIMIT_SECONDS') ?: 5),
+    'max_entries' => (int)(getenv('MAX_ENTRIES') ?: 25)
 ];
+?>
